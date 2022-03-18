69f5127de8baf036636a8b1c0431bf35966e6b91
Adeleke Hammed Lekan <lekhad19@gmail.com>
2022-03-18 17:27:15 -0400

    firebase demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { Course } from './app.model';
 import { Component } from '@angular/core';
-
+import { AngularFireDatabase, AngularFireList  } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // title = 'app';
+  courses: any[];
+  // courses: Observable<any[]>;
+  constructor(db: AngularFireDatabase){
+    // db.list('/courses')
+    //   .subscribe(courses => {
+    //     this.courses = courses;
+    //     console.log(this.courses);
+    // });
+
+    db.list('/courses').valueChanges().subscribe(courses => {
+        this.courses = courses;
+        console.log(this.courses);
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase: {
+    apiKey: "AIzaSyDr7M1-uwJLauLzx1jfIRNtJ0i6qThhu8g",
+    authDomain: "fir-demo-4b7aa.firebaseapp.com",
+    databaseURL: "https://fir-demo-4b7aa-default-rtdb.firebaseio.com",
+    projectId: "fir-demo-4b7aa",
+    storageBucket: "fir-demo-4b7aa.appspot.com",
+    messagingSenderId: "827010142398",
+    appId: "1:827010142398:web:5eeb684a12d5892d86ceef"
+  }
 };
